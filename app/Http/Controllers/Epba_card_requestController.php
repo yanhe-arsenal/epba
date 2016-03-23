8013d14f295178bc692e31f8897496e8e8eeb8af
He Yan <heyan@mt-asa1-vpn236.research.att.com>
2016-03-23 15:59:13 -0400

    more changes, what do you want me to say?

--- app/Http/Controllers/Epba_card_requestController.php
+++ app/Http/Controllers/Epba_card_requestController.php
@@ -54,7 +54,7 @@ class Epba_card_requestController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'recipient_email' => 'required|unique:epba_card_requests|email',
+            'recipient_email' => 'required|unique:epba_card_requests|email|max:20',
         ]);
 
         $request->user()->epba_card_requests()->create([
