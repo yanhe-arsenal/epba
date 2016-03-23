2354f168085bade6fe7a4209cecf3f16675da518
He Yan <heyan@H-135-207-228-226.research.att.com>
2016-03-23 02:28:07 -0400

    more changes

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/tasks';
+    protected $redirectTo = '/epba_card_requests';
 
     /**
      * Create a new authentication controller instance.
