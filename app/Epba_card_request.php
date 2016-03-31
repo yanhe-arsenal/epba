3c436e5f6dac3c7fe18c7898af7da121493cb716
He Yan <heyan@MacBook-Pro-3.local>
2016-03-31 01:07:30 -0400

    more changes, what do you want me to say?

---
+++ app/Epba_card_request.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Epba_card_request extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['recipient_email'];
+
+     /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'user_id' => 'int',
+    ];
+
+    /**
+     * Get the user that owns the epba card request.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+	
+}
