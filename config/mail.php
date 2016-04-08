071c881de6a32258327ceb2a45cf346c5a08fa16
He Yan <heyan@bd-228-16.research.att.com>
2016-04-08 17:30:05 -0400

    more changes, what do you want me to say?

--- config/mail.php
+++ config/mail.php
@@ -1,12 +1,9 @@
 <?php
 return array( 
  'driver' => 'smtp', 
- 'host' => 'mail.myepba.com', 
- 'port' => 2626, 
- 'from' => array('address' => 'myepbaco@myepba.com', 'name' => 'He Yan'), 
- 'username' => 'myepbaco', 
- 'password' => '97Lqn4en8I', 
- 'sendmail' => '/usr/sbin/sendmail -bs', 
- 'pretend' => false, 
+ 'host' => 'smtp.gmail.com', 
+ 'port' => 587, 
+ 'username' => 'hehe.yan@gmail.com', 
+ 'password' => '790926', 
 );
 ?>
---
+++ config/mail_epba.php
@@ -0,0 +1,12 @@
+<?php
+return array( 
+ 'driver' => 'smtp', 
+ 'host' => 'mail.myepba.com', 
+ 'port' => 2626, 
+ 'from' => array('address' => 'myepbaco@myepba.com', 'name' => 'He Yan'), 
+ 'username' => 'myepbaco', 
+ 'password' => '97Lqn4en8I', 
+ 'sendmail' => '/usr/sbin/sendmail -bs', 
+ 'pretend' => false, 
+);
+?>
