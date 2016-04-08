7c0c256624e01ce86b03ab4503bcffc40155a7e8
He Yan <heyan@bd-228-16.research.att.com>
2016-04-08 17:33:26 -0400

    more changes, what do you want me to say?

--- config/mail.php
+++ config/mail.php
@@ -1,7 +1,7 @@
 <?php
 return array( 
  'driver' => 'smtp', 
- 'host' => 'smtp.gmail.com', 
+ 'host' => 'gmail-smtp-msa.l.google.com', 
  'port' => 587, 
  'username' => 'hehe.yan@gmail.com', 
  'password' => '790926', 
