089ccf2a28e39c4d2bb7877822edd11b161a4299
He Yan <heyan@bd-228-16.research.att.com>
2016-04-08 17:19:53 -0400

    more changes, what do you want me to say?

--- config/mail.php
+++ config/mail.php
@@ -1,10 +1,9 @@
 <?php
 return array( 
  'driver' => 'smtp', 
- 'host' => 'server212.webhostingpad.com', 
- 'port' => 465, 
+ 'host' => 'mail.myepba.com', 
+ 'port' => 2626, 
  'from' => array('address' => 'myepbaco@myepba.com', 'name' => 'He Yan'), 
- 'encryption' => 'tls', 
  'username' => 'myepbaco', 
  'password' => '97Lqn4en8I', 
  'sendmail' => '/usr/sbin/sendmail -bs', 
