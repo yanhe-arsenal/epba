55cf69570e3c7288eecbad03f01f9fc36dad7903
He Yan <heyan@bd-228-16.research.att.com>
2016-04-09 01:00:41 -0400

    more changes, what do you want me to say?

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -3,7 +3,6 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Example 2</title>
-    {!! Html::style('assets/css/pdf.css') !!}
   </head>
   <body>
  
