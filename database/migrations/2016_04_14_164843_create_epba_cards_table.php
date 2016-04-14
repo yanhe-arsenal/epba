32aa79fbd66bc2e4ccb8b417589e4460e71b57f2
He Yan <heyan@bd-228-75.research.att.com>
2016-04-14 12:59:53 -0400

    more changes, what do you want me to say?

---
+++ database/migrations/2016_04_14_164843_create_epba_cards_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpbaCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('epba_cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('card_id');
+            $table->timestamps();
+            $table->string('card_recipient_last_name');
+            $table->string('card_recipient_first_name');
+            $table->string('card_recipient_email');
+            $table->string('card_recipient_phone_number');
+            $table->string('card_recipient_birthday');
+            $table->string('card_recipient_address');
+            $table->integer('card_usage')->default(0);
+            $table->index(['card_id', 'card_recipient_email']);		
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('epba_cards');
+    }
+}
