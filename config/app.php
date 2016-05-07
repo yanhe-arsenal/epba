c81a49b66c59fe7de829c3bee54520c0de5c515d
He Yan <heyan@MacBook-Pro-3.home>
2016-05-07 01:38:08 -0400

    more changes, what do you want me to say?

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+	Illuminate\Html\HtmlServiceProvider::class
 
         /*
          * Application Service Providers...
@@ -206,6 +207,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 	'PDF' => Barryvdh\DomPDF\Facade::class,
+	'Html' => 'Illuminate\Html\HtmlFacade',
+	'Form' => 'Illuminate\Html\FormFacade',
 
     ],
 
